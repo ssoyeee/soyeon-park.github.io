579f5591089059a8c1613a09c188f91c664a0158
soyeonee <busanpsy1@gmail.com>
2023-05-11 16:09:06 -0400

    JS: home transparent

--- main.js
+++ main.js
@@ -26,14 +26,20 @@ navbarMenu.addEventListener('click', (event) => {
     }
 });
 
-    //handle click on "contact me" button 
-    const homeContactBtn = document.querySelector('.home__contact');
-    homeContactBtn.addEventListener('click', () => {
-        scrollIntoView('#contact');
-    });
+//Handle click on "contact me" button 
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+      scrollIntoView('#contact');
+});
     
-    function scrollIntoView(selector) {
-        const scrollTo = document.querySelector(selector);
-        scrollTo.scrollIntoView({behavior: "smooth"});
-    }
-    
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    home.style.opacity = 1-window.scrollY/homeHeight; 
+})
